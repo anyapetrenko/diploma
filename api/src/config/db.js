4f62f52feed8a4e8e32434661c047f7636411146
ann_p <91524835+anyapetrenko@users.noreply.github.com>
2024-05-30 01:04:38 +0200

    Add files via upload

---
+++ api/src/config/db.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "password",
+    database: "arena",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: 0,
+    seederStorage: "sequelize",
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "arena",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: 0,
+    seederStorage: "sequelize",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    seederStorage: "sequelize",
+    operatorsAliases: 0,
+    logging: false,
+  },
+};
